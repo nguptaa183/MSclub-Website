8da827131f4bb402ef48dd32eb74dd340f2e54b3
guptan <nguptaa183@gmail.com>
2018-06-01 23:55:48 +0545

    changes

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// navbar scroll
+$(document).ready(function(){
+    $(window).on("scroll",function(){
+      var wn = $(window).scrollTop();
+      if(wn > 215){
+        $(".navbar").css("background","rgba(0,0,0,0.9)");
+      }
+      else{
+        $(".navbar").css("background","rgba(0,0,0,0)");
+      }
+    });
+  });
+  
