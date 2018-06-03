1bac355b7c6d271adf482f8b4fd2fd97181a5d38
guptan <nguptaa183@gmail.com>
2018-06-03 10:16:35 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $(window).on("scroll",function(){
       var wn = $(window).scrollTop();
       if(wn > 215){
-        $(".navbar").css("background","rgba(0,0,0,0.9)");
+        $(".navbar").css("background","#21242A");
       }
       else{
         $(".navbar").css("background","rgba(0,0,0,0)");
