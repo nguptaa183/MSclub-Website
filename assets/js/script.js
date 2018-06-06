84436c49270b0dade06c0fbf4a0ae7c89d777c6a
guptan <nguptaa183@gmail.com>
2018-06-06 20:55:42 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 // navbar scroll
-$(document).ready(function(){
-    $(window).on("scroll",function(){
-      var wn = $(window).scrollTop();
-      if(wn > 215){
-        $(".navbar").css("background","#21242A");
-      }
-      else{
-        $(".navbar").css("background","rgba(0,0,0,0.2)");
-      }
-    });
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var wn = $(window).scrollTop();
+    if (wn > 215) {
+      $(".navbar").css("background", "#21242A");
+    } else {
+      $(".navbar").css("background", "rgba(0,0,0,0)");
+    }
   });
-  
+});
+
+// tooltip
+$(function () {
+  $("[data-toggle='tooltip']").tooltip();
+});
