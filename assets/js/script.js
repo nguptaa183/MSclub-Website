aa82c85f4e69b77af0f5ebdc50737b810602c946
guptan <nguptaa183@gmail.com>
2018-06-07 11:04:30 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,17 @@ $(function () {
 });
 
 // counterstyle
-$('.statistic-counter_two, .statistic-counter, .count-number').counterUp({
-  delay: 10,
-  time: 2000
-});
+ 
+ $(document).ready(function(){
+     $('.counter-value').each(function(){
+         $(this).prop('Counter',0).animate({
+             Counter: $(this).text()
+         },{
+             duration: 3500,
+             easing: 'swing',
+             step: function (now){
+                 $(this).text(Math.ceil(now));
+             }
+         });
+     });
+ });
