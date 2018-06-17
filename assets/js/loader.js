dae4ce84bdf7554ca0e11cbff066df799b6c3ab9
guptan <nguptaa183@gmail.com>
2018-06-17 21:36:55 +0545

    changes

---
+++ assets/js/loader.js
@@ -0,0 +1,12 @@
+// loadbar
+$(document).ready(function(){
+
+    var div_box="<div id='load-screen'><div id='loading' class='img-fluid'></div></div>";
+    $("body").prepend(div_box);
+    
+    $('#load-screen').delay(700).fadeOut(600,function(){
+      $(this).remove();
+    });
+    
+    });
+    
