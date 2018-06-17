03b256c5517be60120c2005d912ead5d275912f9
guptan <nguptaa183@gmail.com>
2018-06-17 06:56:26 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,100 +1,98 @@
+ (function ($) {
+   "use strict"; // Start of use strict
 
-
- (function($) {
-  "use strict"; // Start of use strict
-
-    // Scroll to top button appear
-$(document).ready(function () {
-  $(window).on("scroll", function () {
-    var wn = $(window).scrollTop();
-    if (wn > 100) {
-    $('.scroll-to-top').fadeIn();
-  } else {
-    $('.scroll-to-top').fadeOut();
-  }
-  });
-});
-
-// tooltip
-$(function () {
-  $("[data-toggle='tooltip']").tooltip();
-});
-
-// counterstyle
- 
- $(document).ready(function(){
-     $('.counter-value').each(function(){
-         $(this).prop('Counter',0).animate({
-             Counter: $(this).text()
-         },{
-             duration: 3500,
-             easing: 'swing',
-             step: function (now){
-                 $(this).text(Math.ceil(now));
-             }
-         });
+   // Scroll to top button appear
+   $(document).ready(function () {
+     $(window).on("scroll", function () {
+       var wn = $(window).scrollTop();
+       if (wn > 100) {
+         $('.scroll-to-top').fadeIn();
+       } else {
+         $('.scroll-to-top').fadeOut();
+       }
      });
- });
-  // Closes the sidebar menu
-  $(".menu-toggle").click(function(e) {
-    e.preventDefault();
-    $("#sidebar-wrapper").toggleClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-    $(this).toggleClass("active");
-  });
+   });
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+   // tooltip
+   $(function () {
+     $("[data-toggle='tooltip']").tooltip();
+   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('#sidebar-wrapper .js-scroll-trigger').click(function() {
-    $("#sidebar-wrapper").removeClass("active");
-    $(".menu-toggle").removeClass("active");
-    $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
-  });
+   // counterstyle
 
-  // Scroll to top button appear
-  // $(document).scroll(function() {
-  //   var scrollDistance = $(this).scrollTop();
-  //   if (scrollDistance > 100) {
-  //     $('.scroll-to-top').fadeIn();
-  //   } else {
-  //     $('.scroll-to-top').fadeOut();
-  //   }
-  // });
+   $(document).ready(function () {
+     $('.counter-value').each(function () {
+       $(this).prop('Counter', 0).animate({
+         Counter: $(this).text()
+       }, {
+         duration: 3500,
+         easing: 'swing',
+         step: function (now) {
+           $(this).text(Math.ceil(now));
+         }
+       });
+     });
+   });
+   // Closes the sidebar menu
+   $(".menu-toggle").click(function (e) {
+     e.preventDefault();
+     $("#sidebar-wrapper").toggleClass("active");
+     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+     $(this).toggleClass("active");
+   });
 
-})(jQuery); // End of use strict
+   // Smooth scrolling using jQuery easing
+   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+       var target = $(this.hash);
+       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+       if (target.length) {
+         $('html, body').animate({
+           scrollTop: target.offset().top
+         }, 1000, "easeInOutExpo");
+         return false;
+       }
+     }
+   });
+
+   // Closes responsive menu when a scroll trigger link is clicked
+   $('#sidebar-wrapper .js-scroll-trigger').click(function () {
+     $("#sidebar-wrapper").removeClass("active");
+     $(".menu-toggle").removeClass("active");
+     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+   });
+
+   // Scroll to top button appear
+   // $(document).scroll(function() {
+   //   var scrollDistance = $(this).scrollTop();
+   //   if (scrollDistance > 100) {
+   //     $('.scroll-to-top').fadeIn();
+   //   } else {
+   //     $('.scroll-to-top').fadeOut();
+   //   }
+   // });
+
+ })(jQuery); // End of use strict
 
 
-// maps all commented
-// Disable Google Maps scrolling
-// See http://stackoverflow.com/a/25904582/1607849
-// Disable scroll zooming and bind back the click event
-var onMapMouseleaveHandler = function(event) {
-  var that = $(this);
-  that.on('click', onMapClickHandler);
-  that.off('mouseleave', onMapMouseleaveHandler);
-  that.find('iframe').css("pointer-events", "none");
-}
-var onMapClickHandler = function(event) {
-  var that = $(this);
-  // Disable the click handler until the user leaves the map area
-  that.off('click', onMapClickHandler);
-  // Enable scrolling zoom
-  that.find('iframe').css("pointer-events", "auto");
-  // Handle the mouse leave event
-  that.on('mouseleave', onMapMouseleaveHandler);
-}
-// Enable map zooming with mouse scroll when the user clicks the map
-$('.map').on('click', onMapClickHandler);
+ // maps all commented
+ // Disable Google Maps scrolling
+ // See http://stackoverflow.com/a/25904582/1607849
+ // Disable scroll zooming and bind back the click event
+ var onMapMouseleaveHandler = function (event) {
+   var that = $(this);
+   that.on('click', onMapClickHandler);
+   that.off('mouseleave', onMapMouseleaveHandler);
+   that.find('iframe').css("pointer-events", "none");
+ }
+ var onMapClickHandler = function (event) {
+   var that = $(this);
+   // Disable the click handler until the user leaves the map area
+   that.off('click', onMapClickHandler);
+   // Enable scrolling zoom
+   that.find('iframe').css("pointer-events", "auto");
+   // Handle the mouse leave event
+   that.on('mouseleave', onMapMouseleaveHandler);
+ }
+ // Enable map zooming with mouse scroll when the user clicks the map
+ $('.map').on('click', onMapClickHandler);
