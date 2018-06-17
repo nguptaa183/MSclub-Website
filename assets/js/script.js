eaf1193a0af15b6c3f2f73bdd7b28e1ac23f8ee3
guptan <nguptaa183@gmail.com>
2018-06-17 06:55:59 +0545

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,20 @@
 
+
+ (function($) {
+  "use strict"; // Start of use strict
+
+    // Scroll to top button appear
+$(document).ready(function () {
+  $(window).on("scroll", function () {
+    var wn = $(window).scrollTop();
+    if (wn > 100) {
+    $('.scroll-to-top').fadeIn();
+  } else {
+    $('.scroll-to-top').fadeOut();
+  }
+  });
+});
+
 // tooltip
 $(function () {
   $("[data-toggle='tooltip']").tooltip();
@@ -19,10 +35,6 @@ $(function () {
          });
      });
  });
-
- (function($) {
-  "use strict"; // Start of use strict
-
   // Closes the sidebar menu
   $(".menu-toggle").click(function(e) {
     e.preventDefault();
@@ -53,36 +65,36 @@ $(function () {
   });
 
   // Scroll to top button appear
-  $(document).scroll(function() {
-    var scrollDistance = $(this).scrollTop();
-    if (scrollDistance > 100) {
-      $('.scroll-to-top').fadeIn();
-    } else {
-      $('.scroll-to-top').fadeOut();
-    }
-  });
+  // $(document).scroll(function() {
+  //   var scrollDistance = $(this).scrollTop();
+  //   if (scrollDistance > 100) {
+  //     $('.scroll-to-top').fadeIn();
+  //   } else {
+  //     $('.scroll-to-top').fadeOut();
+  //   }
+  // });
 
 })(jQuery); // End of use strict
 
 
 // maps all commented
-// // Disable Google Maps scrolling
-// // See http://stackoverflow.com/a/25904582/1607849
-// // Disable scroll zooming and bind back the click event
-// var onMapMouseleaveHandler = function(event) {
-//   var that = $(this);
-//   that.on('click', onMapClickHandler);
-//   that.off('mouseleave', onMapMouseleaveHandler);
-//   that.find('iframe').css("pointer-events", "none");
-// }
-// var onMapClickHandler = function(event) {
-//   var that = $(this);
-//   // Disable the click handler until the user leaves the map area
-//   that.off('click', onMapClickHandler);
-//   // Enable scrolling zoom
-//   that.find('iframe').css("pointer-events", "auto");
-//   // Handle the mouse leave event
-//   that.on('mouseleave', onMapMouseleaveHandler);
-// }
-// // Enable map zooming with mouse scroll when the user clicks the map
-// $('.map').on('click', onMapClickHandler);
+// Disable Google Maps scrolling
+// See http://stackoverflow.com/a/25904582/1607849
+// Disable scroll zooming and bind back the click event
+var onMapMouseleaveHandler = function(event) {
+  var that = $(this);
+  that.on('click', onMapClickHandler);
+  that.off('mouseleave', onMapMouseleaveHandler);
+  that.find('iframe').css("pointer-events", "none");
+}
+var onMapClickHandler = function(event) {
+  var that = $(this);
+  // Disable the click handler until the user leaves the map area
+  that.off('click', onMapClickHandler);
+  // Enable scrolling zoom
+  that.find('iframe').css("pointer-events", "auto");
+  // Handle the mouse leave event
+  that.on('mouseleave', onMapMouseleaveHandler);
+}
+// Enable map zooming with mouse scroll when the user clicks the map
+$('.map').on('click', onMapClickHandler);
