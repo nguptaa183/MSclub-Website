437402e666e30be406ba1836ba0c4f0e0908762a
guptan <nguptaa183@gmail.com>
2018-07-08 15:22:28 +0545

    changes

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
 
-  var div_box = "<div id='load-screen'><div id='loading' class='img-responsive'></div></div>";
+  var div_box = "<div id='load-screen'><div id='loading' class='img-fluid'></div></div>";
   $("body").prepend(div_box);
 
   $('#load-screen').delay(700).fadeOut(600, function () {
