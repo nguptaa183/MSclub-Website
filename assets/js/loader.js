db8ca37b306920b68793398b8bdd348c5b16b497
guptan <nguptaa183@gmail.com>
2018-07-08 18:49:57 +0545

    changes

--- assets/js/loader.js
+++ assets/js/loader.js
@@ -1,11 +1,3 @@
-$(document).ready(function () {
-
-  var div_box = "<div id='load-screen'><div id='loading' class='img-fluid'></div></div>";
-  $("body").prepend(div_box);
-
-  $('#load-screen').delay(700).fadeOut(600, function () {
-    $(this).remove();
-  });
-
-
+$(window).load(function(){
+  $('.loader').fadeOut();
 });
